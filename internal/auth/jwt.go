b1d77d134cc589fb1badb2dd90c1c2d873e20367
Zoe <caramel.moustache@orange.fr>
2026-01-28 14:19:08 +0100

    jwt done

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,7 +1,7 @@
 package auth
 
 import (
-	"encoding/hex"
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -10,18 +10,53 @@ import (
 
 func MakeJWT(userID uuid.UUID, tokenSecret string, expireIn time.Duration) (string, error) {
 
-	claim := jwt.RegisteredClaims{
+	claims := jwt.RegisteredClaims{
 		Issuer:    "chirpy",
-		IssuedAt:  jwt.NewNumericDate(time.Now()),
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expireIn)),
-		Subject:   hex.EncodeToString(userID[:]),
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expireIn)),
+		Subject:   userID.String(),
 	}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
-	ss, err := token.SignedString(tokenSecret)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString([]byte(tokenSecret))
 	if err != nil {
 		return "", err
 	}
 
 	return ss, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+
+	claims := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&claims,
+		func(t *jwt.Token) (interface{}, error) {
+			return []byte(tokenSecret), nil
+		},
+	)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	userIDString, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	issuer, err := token.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if issuer != "chirpy" {
+		return uuid.Nil, errors.New("invalid issuer")
+	}
+
+	id, err := uuid.Parse(userIDString)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return id, nil
 }
